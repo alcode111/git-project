dc497773fb09d28d2e7049c9c5f78dab53b47335
Ismail <141173099+alcode111@users.noreply.github.com>
2024-03-06 13:36:41 +0100

    Add all the project files

---
+++ ProjGit/ProjGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjGitApp.swift
+//  ProjGit
+//
+//  Created by Ismaïl LP on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
